33f0b0793ab0a7839f6e4c35e0b6d36cc22da2a0
Dawid Kawka <d.kawka@op.pl>
2022-01-31 18:33:02 +0100

    add last editing in html and js, and end project

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ class Quiz {
         { q: "Ile to jest 10 / 2?", answers: ["4", "5", "4,5"], correctAnswerNum: 1 },
         { q: "Ile to jest 16 + 2?", answers: ["18", "16", "20"], correctAnswerNum: 0 },
         { q: "Ile to jest 8 * 2?", answers: ["18", "10", "16"], correctAnswerNum: 2 },
-
     ];
 
     currentQuestionIndex = -1;
@@ -24,6 +23,8 @@ class Quiz {
     saveAnswerButton = null;
     nextQuestionButton = null;
 
+    modalWindow = null;
+
     init() {
         this.heading = document.querySelector(".alert-heading");
         this.answer0 = document.querySelector("#answer0");
@@ -39,6 +40,13 @@ class Quiz {
         this.saveAnswerButton.addEventListener("click", this.checkAnswer);
         this.nextQuestionButton.addEventListener("click", this.setNextQuestionData);
 
+        this.initModal();
+    }
+
+    initModal = () => {
+        this.modalWindow = new bootstrap.Modal(document.getElementById("modalWindow"));
+
+        document.getElementById("closeModal").addEventListener("click", this.restartQuiz);
     }
 
     checkAnswer = () => {
@@ -50,7 +58,7 @@ class Quiz {
         if (selectedIndex == this.correctAnswerNum) {
             // prawidłowa odpowiedź
             this.userCorrectAnswersNum++;
-            this.userSelectedInput.classList.add("is-valid")
+            this.userSelectedInput.classList.add("is-valid");
         } else {
             // nieprawidłowa odpowiedź
             this.userBadAnswersNum++;
@@ -66,20 +74,21 @@ class Quiz {
     setUserStats = () => {
         document.getElementById("correctAnswers").innerHTML = this.userCorrectAnswersNum;
         document.getElementById("badAnswers").innerHTML = this.userBadAnswersNum;
-
     }
+
 
     setNextQuestionData = () => {
         this.currentQuestionIndex++;
 
         if (this.currentQuestionIndex >= this.questions.length) {
             console.log("Koniec Quizu");
+            this.showModalResults();
             return;
         }
 
         const question = this.questions[this.currentQuestionIndex];
         const qStr = `Pytanie ${this.currentQuestionIndex + 1}
-        z ${this.questions.length}: `;
+                        z ${this.questions.length}: `;
         this.heading.innerHTML = qStr + question.q;
         this.answer0.innerHTML = question.answers[0];
         this.answer1.innerHTML = question.answers[1];
@@ -94,6 +103,29 @@ class Quiz {
 
         this.saveAnswerButton.classList.remove("disabled");
         this.nextQuestionButton.classList.add("disabled");
+    }
+
+    showModalResults = () => {
+        const modalParagraph = document.getElementById("modalResults");
+
+        let information;
+        if (this.userCorrectAnswersNum >= this.userBadAnswersNum) {
+            information = "Brawo! Przynajmniej połowa odpowiedzi jest prawidłowa.";
+        } else {
+            information = "Niestety! Mniej niż połowa odpowiedzi jest prawidłowa.";
+        }
+
+        modalParagraph.innerHTML = information;
+        this.modalWindow.toggle();
+    }
+
+    restartQuiz = () => {
+        this.currentQuestionIndex = -1;
+        this.userCorrectAnswersNum = 0;
+        this.userBadAnswersNum = 0;
+
+        this.setUserStats();
+        this.setNextQuestionData();
     }
 };
 
